6c35735e206fd085289495d990cf05c18a0331e7
wasobi <obi.sierra@gmail.com>
2020-11-24 00:04:25 -0500

    finished v2 sychronization

--- Scripts/script01.sh
+++ Scripts/script01.sh
@@ -1,4 +1,59 @@
 #!/bin/bash
+WIDTH=$1
+HEIGHT=$2
+THREADS=$3
+# verify that the correct number of arguments was passed in
+if [ "$#" -ne 3 ]
+then
+    echo "ERROR: not enough arguments"
+    echo "---------------------------"
+    echo "Usage: $0 <width> <height> <threads>"
+    exit
+else
+    ./cell_v1 $WIDTH $HEIGHT $THREADS
+fi
+
+# pipe commands into the program
+PIP=/tmp/cell
+if [[ ! -p $PIP ]]; then
+    mkfifo $PIP
+fi
+while [[ $CMD != "end" ]]; do
+    echo " Enter a command:"
+    echo "[rule <#>] | [color on] | [color off] | [speedup] | [slowdown] | [end]"
+    read CMD
+    case $CMD in
+        "rule" & [1-4])
+        echo $CMD > $PIP
+        ;;
+        "color on")
+        echo $CMD > $PIP
+        ;;
+        "color off")
+        echo $CMD > $PIP
+        ;;
+        "speedup")
+        echo $CMD > $PIP
+        ;;
+        "slowdown")
+        echo $CMD > $PIP
+        ;;
+        "end"
+        echo $CMD > $PIP
+        ;;
+        *)
+        echo "ERROR: unknown command, please try again."
+        echo "Usage: {rule <#>|color on|color off|speedup|slowdown|end}"
+        ;;
+    esac
+done
+
+# if [[ "$#"]];
+#     then
+#         echo $* > $PIPE
+# else
+
+
 # ---------------------------
 # DIRECTIONS
 # ---------------------------
@@ -9,17 +64,3 @@
 # • Launch a cellular automaton process, specifying the desired width and height;
 # • Communicate with the CA, sending commands grabbed from the standard input.
 # ---------------------------
-
-mkfifo /tmp/cell
-
-if [ ! -d /tmp/Version01 ]; then
-  mkdir -p /tmp/Version01;
-fi
-
-# verify that the correct number of arguments was passed in
-if [ "$#" = "2" ]
-else
-    echo "ERROR: not enough arguments"
-    echo "Usage: $0 <path-to-directory> <filename>"
-    exit
-fi
--- Scripts/script02.sh
+++ Scripts/script02.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+OPTIONS="Hello Quit"
+select opt in $OPTIONS; do
+   if [ "$opt" = "Quit" ]; then
+    echo done
+    exit
+   elif [ "$opt" = "Hello" ]; then
+    echo Hello World
+   else
+    clear
+    echo bad option
+   fi
+done
+
 # ---------------------------
 # EXTRA CREDIT - 12 Points
 # ---------------------------
