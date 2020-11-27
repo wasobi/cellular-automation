98e2e5101128f562ed15f7faa8d66ec95157ed98
wasobi <obi.sierra@gmail.com>
2020-11-27 16:12:49 -0500

    finished v2 sychronization and fixed deadlock

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,7 +3,5 @@ if [ ! -d build ]; then
   mkdir -p build;
 fi
 cd build
-# g++ -Wall -std=c++17 -stdlib=libc++ ../../Version01/main.cpp ../../Version01/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v1
-# ./cell_v1
+g++ -Wall -std=c++17 -stdlib=libc++ ../../Version01/main.cpp ../../Version01/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v1
 g++ -Wall -std=c++17 -stdlib=libc++ ../../Version02/main.cpp ../../Version02/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v2
-./cell_v2
