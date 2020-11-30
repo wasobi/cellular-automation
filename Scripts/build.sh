df7d55042a080d38e674457511879708f5445487
wasobi <obi.sierra@gmail.com>
2020-11-29 22:01:53 -0500

    finished v3 and optimizations

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,3 +5,4 @@ fi
 cd build
 g++ -Wall -std=c++17 -stdlib=libc++ ../../Version01/main.cpp ../../Version01/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v1
 g++ -Wall -std=c++17 -stdlib=libc++ ../../Version02/main.cpp ../../Version02/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v2
+g++ -Wall -std=c++17 -stdlib=libc++ ../../Version03/main.cpp ../../Version03/gl_frontEnd.cpp -lm -framework OpenGL -framework GLUT -o cell_v3
